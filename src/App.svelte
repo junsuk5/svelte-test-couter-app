4de0ff2e3c81e8e38611e3a93d33eccceb95b0fc
Junsuk Oh <junsuk.oh@survivalcoding.com>
2022-07-10 10:22:04 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  let clicks = 0;
+  function increaseClicks() {
+    clicks++;
+  }
+</script>
+
+<main>
+  <h2>클릭 : {clicks}</h2>
+  <button on:click={increaseClicks}>클릭!!</button>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  button {
+    border: 0;
+    background: tomato;
+    color: white;
+    border-radius: 50px;
+    padding: 5px 20px;
+    cursor: pointer;
+  }
+  button:active {
+    opacity: 0.5;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
